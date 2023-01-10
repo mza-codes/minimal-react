19c58d3289e782c08ae5a211f354c3dae6a3598b
mza-codes <muzofficial90@gmail.com>
2023-01-10 06:02:32 +0000

    Ready to Use

---
+++ tailwind.config.cjs
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+      abel: ['Abel', 'sans- serif'],
+      kanit: ['Kanit', 'sans- serif'],
+      poorStory: ['Poor Story', 'cursive'],
+      righteous: ['Righteous', 'cursive'],
+      gloria: ['Gloria Hallelujah', 'cursive'],
+      poppins: ['Poppins', 'sans-serif']
+    },
+  },
+  plugins: [
+    {
+      ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    }
+  ],
+};
